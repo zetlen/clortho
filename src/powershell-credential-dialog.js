619f55449742efd642235cb9629cfb52d0416151
zetlen <zetlen@gmail.com>
2015-12-07 07:48:08 -0600

    fix windows bug not returning promise

--- src/powershell-credential-dialog.js
+++ src/powershell-credential-dialog.js
@@ -14,7 +14,7 @@ const ps = s => new Promise((y, n) =>
 const delimiter = '|||||';
 module.exports = (service, username, message) => {
   message = message || `Please enter your username and password to login to ${service}.`;
-  ps(
+  return ps(
     `$c = Get-Credential -Message \\"${message}\\" -Username ${username}; ` +
     `$c.GetNetworkCredential().username; echo \\"${delimiter}\\"; ` +
     `$c.GetNetworkCredential().password;`
